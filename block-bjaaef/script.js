32a6c560e0bd7b94984a6cd85df4d3db4cf3f1c9
nmn <nmngarg030798@gmail.com>
2022-01-19 23:23:24 +0530

    project completed

--- block-bjaaef/script.js
+++ block-bjaaef/script.js
@@ -1,25 +1,87 @@
-let header = document.querySelector(`header`)
-header.innerText = `👑 Peoples of GOT`
-let ul = document.querySelector(`ul`)
-for(let i=0; i<3;i++){
-    for(let j=0;j<got.houses[i].people.length;j++){
-        if(!(got.houses[i].people[j].name === "Joffrey Baratheon")){
-            let li = document.createElement(`li`);
-            let div = document.createElement(`div`)
-            let imgElm = document.createElement(`img`);
-            imgElm.src = got.houses[i].people[j].image;
-            let name = document.createElement(`h1`)
-            name.innerText = got.houses[i].people[j].name
-            let descr = document.createElement(`p`)
-            descr.innerText = got.houses[i].people[j].description;
-            let btn = document.createElement(`button`)
-            btn.innerText = `Learn More!`
-            div.append(imgElm,name);
-            li.append(div,descr,btn);
-            
-            ul.append(li);
-        }else{
-            break;
+let p = document.querySelector(`header p`)
+p.innerText = `👑 People of GOT`;
+let inputSearch = p.nextElementSibling;
+let ul = document.querySelector(`.ul`);
+let allHouseNames = document.querySelector(`.all-house-names`);
+let styleTag = "";
+
+let allHouses = got.houses.map(house => house);
+let allHousesPeople = got.houses.reduce((acc, cv) => {
+    acc = acc.concat(cv.people);
+    return acc
+}, []);
+
+
+
+
+function houseNameTags(data = allHouses) {
+    allHouseNames.innerHTML = "";
+    data.forEach(house => {
+        let houseName = document.createElement(`li`);
+        houseName.classList.add(`li-style`);
+        houseName.innerText = `${house.name}`;
+
+        if( house === styleTag ){
+            houseName.classList.add(`style-tag`);
+        }else if (styleTag === "input"){
+            houseName.classList.remove(`style-tag`);
         }
-    }
+        
+
+        houseName.addEventListener(`click`, (event) => {
+            data.find(house => {
+                if (event.target.innerText === house.name) {
+                    let clonedHouse = house.people;
+                    styleTag = house;
+                    houseNameTags(allHouses);
+                    allPeopleCards(clonedHouse);
+                }
+            })
+        });
+
+        allHouseNames.append(houseName);
+    })
+};
+houseNameTags();
+
+
+
+function allPeopleCards(data = allHousesPeople) {
+    ul.innerHTML = "";
+    data.forEach(person => {
+        let li = document.createElement(`li`);
+        let divElm = document.createElement(`div`);
+        divElm.classList.add(`div-elm`)
+        let imgElm = document.createElement(`img`);
+        imgElm.src = person.image;
+        let name = document.createElement(`h1`)
+        name.innerText = person.name
+        let descr = document.createElement(`p`)
+        descr.classList.add(`p`)
+        descr.innerText = person.description;
+        let btn = document.createElement(`button`)
+        btn.innerText = `Learn More!`
+        divElm.append(imgElm, name);
+        li.append(divElm, descr, btn);
+
+        ul.append(li);
+    })
+};
+allPeopleCards();
+
+function handleInputSearch(event) {
+    styleTag = "input";
+    let value = event.target.value;
+    let clonedPerson = allHousesPeople.filter(person => (person.name.toLowerCase().includes(`${value.toLowerCase()}`)))
+    allPeopleCards(clonedPerson);
+    houseNameTags(allHouses);
 }
+
+
+inputSearch.addEventListener(`input`, handleInputSearch);
+
+// let demo = got.houses.reduce((acc, cv) => {
+//     acc = acc.concat(cv.people);
+//     return acc
+// }, []);
+// console.log(demo);
